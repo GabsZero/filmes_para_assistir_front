084257d003c3d026f4949f23f7123e0e6dbebbe4
João de Souza <joaogbdesouza@gmail.com>
2025-03-10 22:01:01 -0300

    formulário para enviar o filme para o backend

---
+++ src/app/filmes/criarFilme.js
@@ -0,0 +1,16 @@
+'use server'
+
+export default async function criarFilme(formData) {
+    const response = await fetch(`http://localhost:8080/filmes/`, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ 
+            nome: formData.get('titulo'),
+            tipoID: formData.get('tipo'),
+         }),
+      });
+    const data = await response.json();
+    console.log(data)
+}
