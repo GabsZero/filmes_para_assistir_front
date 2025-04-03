072ca729b0b0e0935625649796faa41317bbd5c6
Johnny <joaogbdesouza@gmail.com>
2025-04-03 16:51:04 -0300

    refazendo a página de criar filmes

--- src/app/filmes/criarFilme.js
+++ src/app/filmes/criarFilme.js
@@ -1,14 +1,14 @@
 'use server'
 
 export default async function criarFilme(formData) {
-  const response = await fetch(`http://localhost:8080/filmes/`, {
+  const response = await fetch(`http://localhost:3333/api/v1/filmes/`, {
     method: 'post',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      nome: formData.get('titulo'),
-      tipoID: formData.get('tipo'),
+      nome: formData.get('nome'),
+      genero_id: formData.get('genero_id'),
     }),
   });
   const data = await response.json();
