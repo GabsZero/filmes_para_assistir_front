52ea9b0defd72ca64ded1c0d589202c9dfa849ab
Johnny <joaogbdesouza@gmail.com>
2025-04-03 16:19:49 -0300

    refazendo a página de filmes assistidos

--- src/app/filmes_assistidos/page.js
+++ src/app/filmes_assistidos/page.js
@@ -1,34 +1,29 @@
-export default async function Home() {
-  const data = await fetch('http://localhost:8080/filmes?assistido=true')
-  const filmesResponse = await data.json()
-  
+import { DateTime } from 'luxon'
+export default async function Page() {
+  const filmesReponse = await fetch('http://localhost:3333/api/v1/filmes?assistido=true')
+  const filmes = await filmesReponse.json()
+
 
   return (
-    <div className="container mx-auto max-w-3xl mt-5">
-      <h1 className="text-2xl font-bold mb-4 text-center">Filmes já assistidos</h1>
-      <div className="bg-white p-12">
-        <div className="space-y-4">
-          {filmesResponse.data.filmes.map((filme) => (
-            <div key={filme.ID} className="flex justify-between p-4 items-center border border-gray-300 rounded-lg shadow-sm">
-              <span className="">
-                {filme.Nome} - {filme.Tipo}
-              </span>
-              <div className="flex space-x-2">
-                <>
-                  <button
-                    className="bg-red-700 hover:bg-red-600 text-white p-4 rounded shadow-md shadow-red-900"
-                  >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                    </svg>
-
-                  </button>
-                </>
-                
-              </div>
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">Lista de Filmes</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {filmes.data.map((filme) => (
+          <div key={filme.id} className="border p-4 rounded-lg shadow">
+            <h2 className="text-xl font-semibold mb-2">{filme.nome}</h2>
+            <p className="text-gray-600 mb-4">Gênero: {filme.genero}</p>
+            <div className="flex gap-2">
+              <p className='text-sm text-gray-500 text-center mb-5'>Assistido em: {DateTime.fromISO(filme.updated_at).toLocaleString(DateTime.DATETIME_MED)}</p>
             </div>
-          ))}
-        </div>
+            <div className="flex gap-2">
+              <button
+                className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+              >
+                Apagar
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
