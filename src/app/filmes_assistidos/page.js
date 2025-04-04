78e383ddacab03cc859180812c13d13c75af961b
Johnny <joaogbdesouza@gmail.com>
2025-04-03 22:08:58 -0300

    corrigindo nome da página

--- src/app/filmes_assistidos/page.js
+++ src/app/filmes_assistidos/page.js
@@ -6,7 +6,7 @@ export default async function Page() {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6">Lista de Filmes</h1>
+      <h1 className="text-3xl font-bold mb-6">Lista de Filmes Assistidos</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {filmes.data.map((filme) => (
           <div key={filme.id} className="border p-4 rounded-lg shadow">
