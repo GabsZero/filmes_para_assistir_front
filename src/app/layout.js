a519d8a462566d5af6e64365057bb2e454f63731
Johnny <joaogbdesouza@gmail.com>
2025-04-03 23:09:19 -0300

    exibindo menu no celular

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
           <div className="mx-auto max-w-2xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="hidden sm:ml-6 sm:block">
+                <div className="sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     <Link href="/" className="rounded-md px-3 py-2 text-lg font-medium text-white hover:bg-gray-700 hover:text-white" aria-current="page">Home</Link>
                     <Link href="/filmes" className="rounded-md px-3 py-2 text-lg font-medium text-white hover:bg-gray-700 hover:text-white" aria-current="page">Novo filme</Link>
